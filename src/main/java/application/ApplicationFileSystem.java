7da5dce0fa736f806e51be1de82bb37f707fe3c5
zglicz <michal.zgliczynski@sonarsource.com>
2025-04-14 11:51:01 +0200

    Initial commit (#1)
    * Initial commit
    
    * force ci re-run
    
    * cirrus
    
    * dont takeout of valut
    
    * more trying
    
    * repush
    
    * add sonar-host-url to cirrus
    
    * add sonar-token
    
    * add j17
    
    * add debug
    
    * random improvements
    
    * more memory
    
    * skip sonar
    
    * without github token
    
    * retry
    
    * more tries
    
    * revert
    
    * remove sonarSkip
    
    * remove extra pom
    
    * try try try
    
    * skip next
    
    * skip sonar analysis
    
    * update pom
    
    * make the goals visible
    
    * Retry with next
    
    * PREQ-590 sonar properties for Next analysis
    
    * fix some sq issues
    
    * skip sonar analysis for now
    
    ---------
    
    Co-authored-by: Julien Carsique <julien.carsique@sonarsource.com>

---
+++ src/main/java/application/ApplicationFileSystem.java
@@ -0,0 +1,40 @@
+/*
+ * RSPEC Maven Plugin
+ * Copyright (C) 2025-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package application;
+
+import domain.Exception;
+
+public class ApplicationFileSystem implements domain.FileSystem {
+
+  private final Host host;
+
+  public ApplicationFileSystem(Host host) {
+    this.host = host;
+  }
+
+  public String resolve(String first, String... more) {
+    return this.host.resolve(first, more);
+  }
+
+  public void write(String filePath, String content) throws Exception {
+    try {
+      this.host.write(filePath, content);
+    } catch (IOException e) {
+      throw new Exception();
+    }
+  }
+}
