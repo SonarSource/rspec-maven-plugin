246d6c9ee95348fb051cd74470c6299d43951e2b
Victor Diez <victor.diez@sonarsource.com>
2025-06-30 11:59:22 +0200

    Fix compilation

--- src/main/java/application/GenerateRegistrarsMojo.java
+++ src/main/java/application/GenerateRegistrarsMojo.java
@@ -19,7 +19,6 @@ package application;
 import domain.Exception;
 import domain.RegistrarsGenerator;
 import infrastructure.JVMHost;
-import java.io.IOException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -69,7 +68,7 @@ public class GenerateRegistrarsMojo extends AbstractMojo {
         this.targetDirectory,
         this.profileName
       );
-    } catch (Exception | IOException e) {
+    } catch (Exception e) {
       throw new MojoExecutionException(e);
     }
   }
